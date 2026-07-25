85e00f3394f6fb6a22a1c9f330d74cc2413403d2
dhyan <87286496+dhyans26@users.noreply.github.com>
2026-07-24 22:45:10 -0400

    feat(experiences): add role-history timeline modal with employment-type badges + also update landing to reflect changes in 2026 and make it easier for future edits w/ more in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,10 +3,14 @@
 export const INFO = {
     name: 'Dhyan',
     education: 'High School',
-    gradeLevel: 'Junior',
+    gradeLevel: 'Senior',
     location: 'Boston',
     github: 'dhyans26',
     email: 'me@dhyans.dev',
     site: 'dhyans.dev',
     repo: 'website-v2',
+    employer: 'The Hack Foundation',
+    employerUrl: 'https://hackclub.com/',
+    program: 'Clubs Program',
+    role:'Lead of Engineering and Infrastructure'
 }
